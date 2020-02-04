075c19f70823cbf6ea8573382d2c45aa26428874
Khabz <khabubundivhu@gmail.com>
2020-02-05 00:05:00 +0200

    Select voices, range and pitch then output results DONE

--- js/main.js
+++ js/main.js
@@ -35,3 +35,63 @@ getVoices();
 if(synth.onvoiceschanged !== undefined) {
     synth.onvoiceschanged = getVoices;
 }
+
+// Speak
+const speak = () => {
+    // Check if speaking
+    if(synth.speaking) {
+        console.error('Already speaking...');
+        return;
+    }
+    // Check if textInput is not null
+    if(textInput.value !== '') {
+        const speakText = new SpeechSynthesisUtterance(textInput.value);
+        // End speaking
+        speakText.onend = e => {
+            console.log('Done speaking');
+        }
+
+        // Speak error
+        speakText.onerror = e => {
+            console.error('Something went wrong');
+        }
+
+        // Selected voice
+        const selectedVoice = voiceSelect.selectedOptions[0]
+        .getAttribute('data-name');
+
+        // Loop through voices
+        voices.forEach(voice => {
+            if(voice.name === selectedVoice) {
+                speakText.voice = voice;
+            }
+        });
+
+        // Set pitch and rate
+        speakText.rate = rate.value;
+        speakText.pitch = pitch.value;
+
+        // Speak
+        synth.speak(speakText);
+    }
+};
+
+// Event Listeners
+
+// Text form submit
+textForm.addEventListener('submit', e => {
+    e.preventDefault();
+    speak();
+    textInput.blur();
+});
+
+// Rate value change
+rate.addEventListener('change', e => rateValue.textContent = 
+rate.value);
+
+// Pitch value change
+pitch.addEventListener('change', e => pitchValue.textContent = 
+pitch.value);
+
+// Voice select change
+voiceSelect.addEventListener('change', e => speak());
